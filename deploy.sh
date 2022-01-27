fdd3f5d35b3b43092889f4418d7c2c5cae985f71
Jenn Bates <jenn.bates@nikolamotor.com>
2022-01-27 13:16:51 -0700

    updating build

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:onomatopoetica/vue-i18n-demo.git master:vue-i18n-demo
+git push -f git@github.com:onomatopoetica/vue-i18n-demo.git master:main
 
 cd -
